098c8d01b279843e32bc12b4b761bd1b31511283
jupottes <juliettepottes@gmail.com>
2019-05-02 16:40:23 -0300

    cpf validate ok test

--- test/validateTest.spec.js
+++ test/validateTest.spec.js
@@ -1,11 +1,26 @@
-var chai = require("chai");
-var expect = chai.expect;
-var cpf = require("../lib/validate")
+const assert = require("chai").assert;
+const cpfValidator = require("../lib/validate");
 
-describe("cpf", () => {
-    describe("cpfValidator", () => {
-        it("cpfValidator", () => {
-            expect(cpf.cpfValidator("9", "6")).to.equal("9")
-        });
+describe("retCpfValidator()", () => {
+    it("deve retornar true", () => {
+    assert.equal(cpfValidator("06414973696"), true);
+    });
+    it("deve retornar false", () => {
+    assert.equal(cpfValidator("06418973696"), false);
+    });
+    it("deve retornar false", () => {
+    assert.equal(cpfValidator("12349054987"), false);
+    });
+    it("deve retornar true", () => {
+    assert.equal(cpfValidator("08172315449"), true);
+    });
+    it("deve retornar false", () => {
+    assert.equal(cpfValidator("081723I5449"), false);
+    });
+    it("deve retornar true", () => {
+    assert.equal(cpfValidator("84958094008"), true);
+    });
+    it("deve retornar false", () => {
+    assert.equal(cpfValidator("aaaaaaaaaaa"), false);
     });
 });
